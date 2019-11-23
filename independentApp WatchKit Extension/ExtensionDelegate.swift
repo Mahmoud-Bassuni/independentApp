e9b2aa08fa056feaa23b2ce2c5ccd4529f617510
mahmoud gaber <mahmoudbasuni90@gmail.com>
2019-11-23 23:53:47 +0200

    first init

--- independentApp WatchKit Extension/ExtensionDelegate.swift
+++ independentApp WatchKit Extension/ExtensionDelegate.swift
@@ -7,11 +7,45 @@
 //
 
 import WatchKit
-
+import UserNotifications
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
     func applicationDidFinishLaunching() {
-        // Perform any final initialization of your application.
+        let center = UNUserNotificationCenter.current()
+                     center.delegate = self
+      //  WKExtension.shared().registerForRemoteNotifications()
+      //  setUpNotificationPreferences()
+        setUpNotificationPreferences()
+        addLocalNotification()
+    }
+
+    func addLocalNotification()  {
+       // Configure the notification's payload.
+       let content = UNMutableNotificationContent()
+        content.title = "hello" //NSString.localizedUserNotificationString(forKey: "Hello!", arguments: nil)
+        content.body = "body" //NSString.localizedUserNotificationString(forKey: "Hello_message_body", arguments: nil)
+        content.sound = UNNotificationSound.default
+
+       // Deliver the notification in five seconds.
+       let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+       let request = UNNotificationRequest(identifier: "FiveSecond", content: content, trigger: trigger) // Schedule the notification.
+       let center = UNUserNotificationCenter.current()
+       center.add(request) { (error : Error?) in
+            if let theError = error {
+                // Handle any errors
+            }
+       }
+    }
+    func didRegisterForRemoteNotifications(withDeviceToken deviceToken: Data)
+
+    {
+        print(deviceToken.base64EncodedString())
+
+    }
+     func didFailToRegisterForRemoteNotificationsWithError(_ error: Error)
+
+    {
+
     }
 
     func applicationDidBecomeActive() {
@@ -54,3 +88,64 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
     }
 
 }
+extension ExtensionDelegate : UNUserNotificationCenterDelegate{
+    // recive notification
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
+    {
+        completionHandler(.alert)
+        // SoundManagement.shared.playSound()
+    }
+    func didReceive(_ notification: UNNotification, withCompletion completionHandler: @escaping (WKUserNotificationInterfaceType) -> Swift.Void) {
+        //SoundManagement.shared.playSound()
+    }
+
+    // recive notification with data
+    func didReceiveRemoteNotification(_ userInfo: [AnyHashable : Any],
+                                      fetchCompletionHandler completionHandler: @escaping (WKBackgroundFetchResult) -> Void)
+    {
+
+
+        completionHandler(.newData)
+    }
+
+
+
+    func application(_ notification: UNNotification, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (WKBackgroundFetchResult) -> Void) {
+
+    }
+
+    // user open notification
+    // MARK: UNUserNotificationCenterDelegate
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        NSLog("\(#function)")
+        completionHandler()
+    }
+
+
+    func setUpNotificationPreferences() {
+
+
+
+
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound], completionHandler: { (granted, error) in
+           if let theError = error {
+               NSLog("\(#function) error when request localNotification \(theError.localizedDescription)")
+           } else {
+               if !granted {
+                   NSLog("\(#function) Warning local notification not granted")
+               }
+           }
+
+        })
+
+        let notificationCategory = UNNotificationCategory(identifier: "GENERAL", actions: [], intentIdentifiers: [],  options: [])
+        let categories: Set = [notificationCategory]
+        UNUserNotificationCenter.current().setNotificationCategories(categories)
+
+        }
+    }
+
+
+
